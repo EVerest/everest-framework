ab304bd058281271977790a3a3a22486ce89598d
aw <aw@pionix.de>
2022-04-25 22:14:35 +0100

    Refactored tests and code coverage
    - catch2 tests are now split into one main and the specific test case
      compilation units
    - code coverage now uses ctest as the executable and excludes the build
      and tests directories for coverage reports
    
    Signed-off-by: aw <aw@pionix.de>

--- tests/test_config.cpp
+++ tests/test_config.cpp
@@ -1,6 +1,5 @@
 // SPDX-License-Identifier: Apache-2.0
 // Copyright 2020 - 2021 Pionix GmbH and Contributors to EVerest
-#define CATCH_CONFIG_MAIN
 #include <catch2/catch.hpp>
 
 #include <framework/everest.hpp>
