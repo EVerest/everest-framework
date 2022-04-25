8b8c0540bc9ec16962a647a7f3f805a61b5d757b
aw <aw@pionix.de>
2022-04-25 22:26:29 +0100

    Fix SPDX years
    Signed-off-by: aw <aw@pionix.de>

--- tests/test_config.cpp
+++ tests/test_config.cpp
@@ -1,5 +1,5 @@
 // SPDX-License-Identifier: Apache-2.0
-// Copyright 2020 - 2021 Pionix GmbH and Contributors to EVerest
+// Copyright 2020 - 2022 Pionix GmbH and Contributors to EVerest
 #include <catch2/catch.hpp>
 
 #include <framework/everest.hpp>
