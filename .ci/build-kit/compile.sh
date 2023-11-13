621213a41ffe1e2db12249824f271bd40a9752c5
Dima Dorezyuk <ddo@qwello.eu>
2023-11-13 16:08:16 +0100

    CI: build the everest-framework in the ci
    Signed-off-by: Dima Dorezyuk <ddo@qwello.eu>

---
+++ .ci/build-kit/compile.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+set -e
+
+# Install coverage dependencies
+apt update && apt install -y gcovr lcov
+
+# Install Rust
+curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh -s -- -y
+source "$HOME/.cargo/env"
+
+cmake \
+    -B build \
+    -S "$EXT_MOUNT" \
+    -G Ninja \
+    -DEVC_ENABLE_CCACHE=1 \
+    -DISO15118_2_GENERATE_AND_INSTALL_CERTIFICATES=OFF \
+    -DBUILD_TESTING=ON \
+    -DEVEREST_ENABLE_RS_SUPPORT=ON
+
+ninja -j$(nproc) -C build && ninja test -C build
