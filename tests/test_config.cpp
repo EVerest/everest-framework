c2929634ee9c931d4c843980e15bc5e1a758c272
Kai-Uwe Hermann <kai-uwe.hermann@pionix.de>
2024-08-30 16:02:20 +0200

    Use auto in tests
    Signed-off-by: Kai-Uwe Hermann <kai-uwe.hermann@pionix.de>

--- tests/test_config.cpp
+++ tests/test_config.cpp
@@ -9,7 +9,7 @@
 namespace fs = std::filesystem;
 
 SCENARIO("Check ManagerSettings Constructor", "[!throws]") {
-    std::string bin_dir = Everest::tests::get_bin_dir().string() + "/";
+    auto bin_dir = Everest::tests::get_bin_dir().string() + "/";
     GIVEN("An invalid prefix, but a valid config file") {
         THEN("It should throw BootException") {
             CHECK_THROWS_AS(
@@ -57,31 +57,31 @@ SCENARIO("Check ManagerSettings Constructor", "[!throws]") {
     }
 }
 SCENARIO("Check Config Constructor", "[!throws]") {
-    std::string bin_dir = Everest::tests::get_bin_dir().string() + "/";
+    auto bin_dir = Everest::tests::get_bin_dir().string() + "/";
     GIVEN("A config without modules") {
-        std::shared_ptr<Everest::ManagerSettings> rs = std::make_shared<Everest::ManagerSettings>(
+        auto rs = std::make_shared<Everest::ManagerSettings>(
             Everest::ManagerSettings(bin_dir + "empty_config/", bin_dir + "empty_config/config.yaml"));
-        Everest::ManagerConfig config = Everest::ManagerConfig(rs);
+        auto config = Everest::ManagerConfig(rs);
         THEN("It should not contain the module some_module") {
             CHECK(!config.contains("some_module"));
         }
     }
     GIVEN("A config file referencing a non existent module") {
-        std::shared_ptr<Everest::ManagerSettings> rs = std::make_shared<Everest::ManagerSettings>(
+        auto rs = std::make_shared<Everest::ManagerSettings>(
             Everest::ManagerSettings(bin_dir + "missing_module/", bin_dir + "missing_module/config.yaml"));
         THEN("It should throw Everest::EverestConfigError") {
             CHECK_THROWS_AS(Everest::ManagerConfig(rs), Everest::EverestConfigError);
         }
     }
     GIVEN("A config file using a module with broken manifest (missing meta data)") {
-        std::shared_ptr<Everest::ManagerSettings> rs = std::make_shared<Everest::ManagerSettings>(
+        auto rs = std::make_shared<Everest::ManagerSettings>(
             Everest::ManagerSettings(bin_dir + "broken_manifest_1/", bin_dir + "broken_manifest_1/config.yaml"));
         THEN("It should throw Everest::EverestConfigError") {
             CHECK_THROWS_AS(Everest::ManagerConfig(rs), Everest::EverestConfigError);
         }
     }
     GIVEN("A config file using a module with broken manifest (empty file)") {
-        std::shared_ptr<Everest::ManagerSettings> rs = std::make_shared<Everest::ManagerSettings>(
+        auto rs = std::make_shared<Everest::ManagerSettings>(
             Everest::ManagerSettings(bin_dir + "broken_manifest_2/", bin_dir + "broken_manifest_2/config.yaml"));
         THEN("It should throw Everest::EverestConfigError") {
             // FIXME: an empty manifest breaks the test?
@@ -89,75 +89,72 @@ SCENARIO("Check Config Constructor", "[!throws]") {
         }
     }
     GIVEN("A config file using a module with broken manifest (broken module config)") {
-        std::shared_ptr<Everest::ManagerSettings> rs = std::make_shared<Everest::ManagerSettings>(
+        auto rs = std::make_shared<Everest::ManagerSettings>(
             Everest::ManagerSettings(bin_dir + "broken_manifest_3/", bin_dir + "broken_manifest_3/config.yaml"));
         THEN("It should throw Everest::EverestConfigError") {
             CHECK_THROWS_AS(Everest::ManagerConfig(rs), Everest::EverestConfigError);
         }
     }
     GIVEN("A config file using a module with broken manifest (broken implementation config)") {
-        std::shared_ptr<Everest::ManagerSettings> rs = std::make_shared<Everest::ManagerSettings>(
+        auto rs = std::make_shared<Everest::ManagerSettings>(
             Everest::ManagerSettings(bin_dir + "broken_manifest_4/", bin_dir + "broken_manifest_4/config.yaml"));
         THEN("It should throw Everest::EverestConfigError") {
             CHECK_THROWS_AS(Everest::ManagerConfig(rs), Everest::EverestConfigError);
         }
     }
     GIVEN("A config file with an unknown implementation config") {
-        std::shared_ptr<Everest::ManagerSettings> rs = std::make_shared<Everest::ManagerSettings>(
+        auto rs = std::make_shared<Everest::ManagerSettings>(
             Everest::ManagerSettings(bin_dir + "unknown_impls/", bin_dir + "unknown_impls/config.yaml"));
         THEN("It should throw Everest::EverestConfigError") {
             CHECK_THROWS_AS(Everest::ManagerConfig(rs), Everest::EverestConfigError);
         }
     }
     GIVEN("A config file with an missing config entry") {
-        std::shared_ptr<Everest::ManagerSettings> rs = std::make_shared<Everest::ManagerSettings>(
+        auto rs = std::make_shared<Everest::ManagerSettings>(
             Everest::ManagerSettings(bin_dir + "missing_config_entry/", bin_dir + "missing_config_entry/config.yaml"));
         THEN("It should throw Everest::EverestConfigError") {
             CHECK_THROWS_AS(Everest::ManagerConfig(rs), Everest::EverestConfigError);
         }
     }
     GIVEN("A config file with an missing implementation config entry") {
-        std::shared_ptr<Everest::ManagerSettings> rs =
-            std::make_shared<Everest::ManagerSettings>(Everest::ManagerSettings(
-                bin_dir + "missing_impl_config_entry/", bin_dir + "missing_impl_config_entry/config.yaml"));
+        auto rs = std::make_shared<Everest::ManagerSettings>(Everest::ManagerSettings(
+            bin_dir + "missing_impl_config_entry/", bin_dir + "missing_impl_config_entry/config.yaml"));
         THEN("It should throw Everest::EverestConfigError") {
             CHECK_THROWS_AS(Everest::ManagerConfig(rs), Everest::EverestConfigError);
         }
     }
     GIVEN("A config file with an invalid type of an implementation config entry") {
-        std::shared_ptr<Everest::ManagerSettings> rs =
-            std::make_shared<Everest::ManagerSettings>(Everest::ManagerSettings(
-                bin_dir + "invalid_config_entry_type/", bin_dir + "invalid_config_entry_type/config.yaml"));
+        auto rs = std::make_shared<Everest::ManagerSettings>(Everest::ManagerSettings(
+            bin_dir + "invalid_config_entry_type/", bin_dir + "invalid_config_entry_type/config.yaml"));
         THEN("It should throw Everest::EverestConfigError") {
             CHECK_THROWS_AS(Everest::ManagerConfig(rs), Everest::EverestConfigError);
         }
     }
     GIVEN("A config file using a module with an invalid interface (missing "
           "interface)") {
-        std::shared_ptr<Everest::ManagerSettings> rs = std::make_shared<Everest::ManagerSettings>(
+        auto rs = std::make_shared<Everest::ManagerSettings>(
             Everest::ManagerSettings(bin_dir + "missing_interface/", bin_dir + "missing_interface/config.yaml"));
         THEN("It should throw Everest::EverestConfigError") {
             CHECK_THROWS_AS(Everest::ManagerConfig(rs), Everest::EverestConfigError);
         }
     }
     GIVEN("A valid config") {
-        std::shared_ptr<Everest::ManagerSettings> rs = std::make_shared<Everest::ManagerSettings>(
+        auto rs = std::make_shared<Everest::ManagerSettings>(
             Everest::ManagerSettings(bin_dir + "valid_config/", bin_dir + "valid_config/config.yaml"));
         THEN("It should not throw at all") {
             CHECK_NOTHROW(Everest::ManagerConfig(rs));
         }
     }
     GIVEN("A valid config with a valid module") {
-        std::shared_ptr<Everest::ManagerSettings> rs = std::make_shared<Everest::ManagerSettings>(
+        auto rs = std::make_shared<Everest::ManagerSettings>(
             Everest::ManagerSettings(bin_dir + "valid_module_config/", bin_dir + "valid_module_config/config.yaml"));
         THEN("It should not throw at all") {
             CHECK_NOTHROW(Everest::ManagerConfig(rs));
         }
     }
     GIVEN("A valid config with a valid module and a user-config applied") {
-        std::shared_ptr<Everest::ManagerSettings> rs =
-            std::make_shared<Everest::ManagerSettings>(Everest::ManagerSettings(
-                bin_dir + "valid_module_config_userconfig/", bin_dir + "valid_module_config_userconfig/config.yaml"));
+        auto rs = std::make_shared<Everest::ManagerSettings>(Everest::ManagerSettings(
+            bin_dir + "valid_module_config_userconfig/", bin_dir + "valid_module_config_userconfig/config.yaml"));
         THEN("It should not throw at all") {
             CHECK_NOTHROW([&]() {
                 auto mc = Everest::ManagerConfig(rs);
@@ -167,9 +164,8 @@ SCENARIO("Check Config Constructor", "[!throws]") {
         }
     }
     GIVEN("A valid config with a valid module and enabled schema validation") {
-        std::shared_ptr<Everest::ManagerSettings> rs =
-            std::make_shared<Everest::ManagerSettings>(Everest::ManagerSettings(
-                bin_dir + "valid_module_config_validate/", bin_dir + "valid_module_config_validate/config.yaml"));
+        auto rs = std::make_shared<Everest::ManagerSettings>(Everest::ManagerSettings(
+            bin_dir + "valid_module_config_validate/", bin_dir + "valid_module_config_validate/config.yaml"));
         THEN("It should not throw at all") {
             CHECK_NOTHROW([&]() {
                 auto mc = Everest::ManagerConfig(rs);
@@ -184,7 +180,7 @@ SCENARIO("Check Config Constructor", "[!throws]") {
         }
     }
     GIVEN("A valid config with a valid module serialized") {
-        std::shared_ptr<Everest::ManagerSettings> rs = std::make_shared<Everest::ManagerSettings>(
+        auto rs = std::make_shared<Everest::ManagerSettings>(
             Everest::ManagerSettings(bin_dir + "valid_module_config/", bin_dir + "valid_module_config/config.yaml"));
         THEN("It should not throw at all") {
             CHECK_NOTHROW([&]() {
@@ -196,9 +192,8 @@ SCENARIO("Check Config Constructor", "[!throws]") {
         }
     }
     GIVEN("A valid config in legacy json format with a valid module") {
-        std::shared_ptr<Everest::ManagerSettings> rs =
-            std::make_shared<Everest::ManagerSettings>(Everest::ManagerSettings(
-                bin_dir + "valid_module_config_json/", bin_dir + "valid_module_config_json/config.json"));
+        auto rs = std::make_shared<Everest::ManagerSettings>(Everest::ManagerSettings(
+            bin_dir + "valid_module_config_json/", bin_dir + "valid_module_config_json/config.json"));
         THEN("It should not throw at all") {
             CHECK_NOTHROW(Everest::ManagerConfig(rs));
         }
