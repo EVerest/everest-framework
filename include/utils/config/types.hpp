504ffae9db57b5368a2b3d3f0440e853eddf8029
Kai-Uwe Hermann <kai-uwe.hermann@pionix.de>
2025-04-09 23:58:18 +0200

    Move Mapping, ModuleTierMappings, Requirement and Fulfillment to utils/config/types.hpp
    Signed-off-by: Kai-Uwe Hermann <kai-uwe.hermann@pionix.de>

--- include/utils/config/types.hpp
+++ include/utils/config/types.hpp
@@ -5,18 +5,76 @@
 
 #include <cstdint>
 #include <filesystem>
+#include <map>
 #include <optional>
 #include <string>
-#include <map>
 #include <variant>
 #include <vector>
+
+/// \brief A Mapping that can be used to map a module or implementation to a specific EVSE or optionally to a Connector
+struct Mapping {
+    int evse;                     ///< The EVSE id
+    std::optional<int> connector; ///< An optional Connector id
+
+    Mapping(int evse) : evse(evse) {
+    }
+
+    Mapping(int evse, int connector) : evse(evse), connector(connector) {
+    }
+};
+
+/// \brief Writes the string representation of the given Mapping \p mapping to the given output stream \p os
+/// \returns an output stream with the Mapping written to
+inline std::ostream& operator<<(std::ostream& os, const Mapping& mapping) {
+    os << "Mapping(evse: " << mapping.evse;
+    if (mapping.connector.has_value()) {
+        os << ", connector: " << mapping.connector.value();
+    }
+    os << ")";
+
+    return os;
+}
+
+/// \brief Writes the string representation of the given Mapping \p mapping to the given output stream \p os
+/// \returns an output stream with the Mapping written to
+inline std::ostream& operator<<(std::ostream& os, const std::optional<Mapping>& mapping) {
+    if (mapping.has_value()) {
+        os << mapping.value();
+    } else {
+        os << "Mapping(charging station)";
+    }
+
+    return os;
+}
+
+/// \brief A 3 tier mapping for a module and its individual implementations
+struct ModuleTierMappings {
+    std::optional<Mapping> module; ///< Mapping of the whole module to an EVSE id and optional Connector id. If this is
+                                   ///< absent the module is assumed to be mapped to the whole charging station
+    std::unordered_map<std::string, std::optional<Mapping>>
+        implementations; ///< Mappings for the individual implementations of the module
+};
+
+struct Requirement {
+    std::string id;
+    size_t index = 0;
+};
+
+bool operator<(const Requirement& lhs, const Requirement& rhs);
+
+/// \brief A Fulfillment relates a Requirement to its connected implementation, identified via its module and
+/// implementation id.
+struct Fulfillment {
+    std::string module_id;
+    std::string implementation_id;
+    Requirement requirement;
+};
 
 namespace everest::config {
 
 namespace fs = std::filesystem;
 
 struct ConfigurationParameter;
-struct ModuleConfig;
 
 using ConfigEntry = std::variant<std::string, bool, int, double>;
 using ImplementationIdentifier = std::string;
@@ -75,35 +133,22 @@ struct ConfigurationParameter {
     ConfigEntry get_typed_value() const;
 };
 
+// TODO: find a better name for this?
+struct ModuleConfig {
+    bool standalone;
+    std::string module_name;
+    std::string module_id;
+    std::optional<std::string> capabilites;
+    ModuleConfigurationParameters configuration_parameters; // contains: config_module and config_implementations
+                                                            // as well as the upcoming "config" key
+    // TODO: add missing TelemetryConfig
+    std::map<std::string, std::vector<Fulfillment>> connections; // TODO: typedef
+    ModuleTierMappings mapping;
+};
+
 struct EverestConfig {
     Settings settings;
     std::vector<ModuleConfig> module_configs;
-};
-
-struct ModuleTierMapping {
-    uint32_t evse_id;
-    std::string implementation_id;
-    std::optional<uint32_t> connector_id;
-};
-
-struct ModuleInfo {
-    std::string module_id;
-    std::string module_name;
-    bool standalone;
-    std::optional<std::string> capabilites;
-};
-
-struct ModuleConnection {
-    std::string requirement_name;
-    std::string implementation_id;
-    std::string module_id;
-};
-
-struct ModuleConfig {
-    ModuleInfo module_info;
-    ModuleConfigurationParameters module_configuration_parameters;
-    std::vector<ModuleConnection> module_connections;
-    std::vector<ModuleTierMapping> module_tier_mappings;
 };
 
 Datatype string_to_datatype(const std::string& str);
@@ -112,4 +157,4 @@ std::string datatype_to_string(const Datatype datatype);
 Mutability string_to_mutability(const std::string& str);
 std::string mutability_to_string(const Mutability mutability);
 
-}
+} // namespace everest::config
